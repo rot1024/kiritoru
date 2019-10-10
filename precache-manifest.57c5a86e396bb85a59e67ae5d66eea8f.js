ab46ae951fc35395b3b02065a1412fa3457790ee
rot1024 <aayhrot@gmail.com>
2019-10-10 16:05:53 +0900

    Updates

---
+++ precache-manifest.57c5a86e396bb85a59e67ae5d66eea8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cceadc7efe2284969948b6c162774a",
+    "url": "/kiritoru/index.html"
+  },
+  {
+    "revision": "d549474aee1cc93a0f55",
+    "url": "/kiritoru/static/css/main.5ba81a30.chunk.css"
+  },
+  {
+    "revision": "0dba64f35b6436c35a37",
+    "url": "/kiritoru/static/js/2.bd81354c.chunk.js"
+  },
+  {
+    "revision": "d549474aee1cc93a0f55",
+    "url": "/kiritoru/static/js/main.ec62c484.chunk.js"
+  },
+  {
+    "revision": "87e0cbaa52f1d9f1408c",
+    "url": "/kiritoru/static/js/runtime-main.5bd8d834.js"
+  }
+]);
