7c12b34b9abd188ffa8c28a918d1dd97223b19da
rot1024 <aayhrot@gmail.com>
2019-10-10 16:01:08 +0900

    add glur

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+module "glur" {
+  const glur: (
+    src: Uint8ClampedArray,
+    width: number,
+    height: number,
+    radius: number
+  ) => void;
+
+  export default glur;
+}
